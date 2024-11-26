0bde414739747e0564b5baee5693256b270cf2e7
Belen Chiarino <beluchiarino@hotmail.com>
2024-11-26 16:53:11 +0100

    styling index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_11_26_141823) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
