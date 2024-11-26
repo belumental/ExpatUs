db596736c9e745c4ca6afcd32ecda09ca7431416
Lintong Jin <Lt.jin@hotmail.com>
2024-11-26 14:37:45 +0100

    add seed file and add description colum to chat table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts("destory all data we have already...")
+User.destroy_all
+Chat.destroy_all
+
+puts("seed user...")
+users = []
+5.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456'
+  )
+  users << user
+end
+
+puts("seed 5 users into database.")
+puts("seed chat...")
+Chat.create(user_id: users.sample.id, title: "Jobs in Amsterdam" , location: "Amsterdam Nieuw-West", category: "Jobs", description:
+                    "Are you looking for a job in Amsterdam? Do you know of any positions available? Join this chat and share tips or information about the job market.")
+Chat.create(user_id: users.sample.id, title: "Social Life" , location: "Amsterdam Zuid", category: "Life", description:
+                    "Find out about events happening around you, make new friends and discover new places.")
+Chat.create(user_id: users.sample.id, title: "Second Hand Stuff" , location: "Amsterdam West", category: "Life", description:
+                    "Buy, sell or trade anything here. Furniture, clothes and much more.")
+puts("done")
