8de09bd1442da341aaa916381fae405b2c9a45a4
Belen Chiarino <beluchiarino@hotmail.com>
2024-11-27 17:03:08 +0100

    cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, only: :new
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  #allow_browser versions: :modern
 
   def after_sign_in_path_for(resource)
     list_path
