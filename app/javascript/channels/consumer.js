9668ff0a0f0ee060cb59df3acbe56d74c2386a91
Lintong Jin <Lt.jin@hotmail.com>
2024-12-01 20:25:58 +0100

    fixed heroku

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,7 +1,7 @@
 import { createConsumer } from "solid_cable"
 // export default createConsumer()
 
-const host = window.location.origin.replace(/^http/, "ws");
+const host = window.location.origin.replace(/^https/, "wss");
 const consumer = createConsumer(`${host}/cable`);
 
 export default consumer;
