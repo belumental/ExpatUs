53d4431851c618ac21eb9e7ff1941c55788f68b8
Imer Durgut <idurgut@gmail.com>
2024-12-03 11:53:37 +0100

    created chats showed

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,4 @@
 class ChatsController < ApplicationController
-  # before_action :authenticate_user!, only: :new
-  # def index
-  # end
-
   before_action :authenticate_user!, only: [:new, :create, :show]
 
   def new
@@ -47,7 +43,7 @@ class ChatsController < ApplicationController
 
       # @chats = User.find(current_user.id).chats
       @chats_with_messages_count = []
-      online_num = self.compute_online_user_num_on_chat.to_h
+      online_num = compute_online_user_num_on_chat.to_h
       chats.each_with_index do |chat, index|
         @chats_with_messages_count[index] = chat.attributes.dup
         @chats_with_messages_count[index]["msgcount"] = Message.where(chat_id: chat.id).count
@@ -58,8 +54,9 @@ class ChatsController < ApplicationController
         end
       end
     else
-      self.list
+      list
     end
+    @created_chats = Chat.where(user_id: current_user.id)
   end
 
   def list
@@ -73,51 +70,9 @@ class ChatsController < ApplicationController
   end
 
   def created_by_user
-    def list_by_user
-      unless params[:query].present?
-        my_messages = Message.where(user_id: current_user.id)
-
-        # to get the chats we clicked 'join' for
-        chats = current_user.chats
-
-        # to get the chats we have written a message in
-        my_messages.each do |my_message|
-          unless chats.include?(my_message.chat)
-            chats.push(my_message.chat)
-          end
-        end
-
-        # @chats = User.find(current_user.id).chats
-        @chats_with_messages_count = []
-        online_num = self.compute_online_user_num_on_chat.to_h
-        chats.each_with_index do |chat, index|
-          @chats_with_messages_count[index] = chat.attributes.dup
-          @chats_with_messages_count[index]["msgcount"] = Message.where(chat_id: chat.id).count
-          online_num.keys.each do |key|
-            if @chats_with_messages_count[index]['id'] == key
-              @chats_with_messages_count[index]["online_num"] = online_num[key]
-            end
-          end
-        end
-      else
-        self.list
-      end
-    end
-
-    def list
-      @user = current_user
-      @chats = Chat.all
-      if params[:query].present?
-        @chats = Chat.search_by_title_and_synopsis(params[:query])
-      else
-        @chats = Chat.all
-      end
-    end
-
-    def created_by_user
-      @created_chats = Chat.where(user_id: current_user.id)
-    end
+    @created_chats = Chat.where(user_id: current_user.id)
   end
+
 
   private
 
