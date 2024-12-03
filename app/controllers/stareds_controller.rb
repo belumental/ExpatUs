51f5f7c93396318f385f1accb00ee4016cf3a3a0
Lintong Jin <Lt.jin@hotmail.com>
2024-12-03 09:05:20 +0100

    add stareds

--- app/controllers/stareds_controller.rb
+++ app/controllers/stareds_controller.rb
@@ -1,17 +1,39 @@
 class StaredsController < ApplicationController
+
+  def index
+    @stareds = Stared.where(user_id: current_user.id)
+  end
+
   def show
     @starred_messages = current_user.stareds.includes(:message_id).map(&:message_id)
     render json: { starred_messages: @starred_messages }
   end
 
   def create
-    @message = Message.find(params[:message_id])
-    @starred_message = current_user.stareds.find(params[:message_id])
-
-    if @starred_message.save
+    transaction_success = false
+    begin
+      ActiveRecord::Base.transaction do
+        params[:messageIds].each do |msgId|
+          Stared.create!(user_id: current_user.id, message_id: msgId)
+        end
+        transaction_success = true
+      end
+    rescue => e
+      puts "Transaction failed: #{e.message}"
+    end
+    if transaction_success
       render json: { starred: true }
     else
-      render json: { error: @starred.errors.full_messages }, status: :unprocessable_entity
+      render json: { starred: false }, status: :unprocessable_entity
     end
+    # Rails.logger.error "xxxxxxxxx: #{params[:messageIds]}"
+    # @message = Message.find(params[:message_id])
+    # @starred_message = current_user.stareds.find(params[:message_id])
+
+    # if @starred_message.save
+    #   render json: { starred: true }
+    # else
+    #   render json: { error: @starred.errors.full_messages }, status: :unprocessable_entity
+    # end
   end
 end
--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -1,8 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
+
+function fadeOut(element) {
+  if(element.style.opacity !=0){
+    let speed = speed || 30 ;
+    let num = 10;
+    let st = setInterval(function(){
+      num--;
+      element.style.opacity = num / 10 ;
+      if(num<=0)  { clearInterval(st); }
+    },speed);
+  }
+}
 // Connects to data-controller="message"
 export default class extends Controller {
-  static values = { userId: Number }
+  static values = { userId: Number, msgId: Number }
+  static targets = ['checkDivs', 'checks', 'checklabel', 'success']
   connect() {
     const currentUserId = parseInt(document.body.dataset.currentUserId, 10);
     if (this.userIdValue === currentUserId) {
@@ -14,4 +27,58 @@ export default class extends Controller {
     }
     this.element.scrollIntoView({ behavior: 'smooth' }); // scroll to the bottom of the page
   }
+
+  saveChecked(event) {
+    if(event.target.checked == true){
+      this.checkDivsTargets.forEach(function (element) {
+        element.classList.remove('d-none')
+      });
+      this.checklabelTarget.innerText = "Confirm"
+    }else{
+      let checkedTags = []
+      this.checksTargets.forEach((ele) => {
+        if(ele.checked == true){
+          checkedTags.push(ele)
+        }
+      })
+      console.log(this.msgIdValue)
+      if (checkedTags == 0){
+        return
+      }
+      let messageIds = []
+      checkedTags.forEach((checkedTag) => {
+        messageIds.push(checkedTag.value)
+      })
+      const data = { messageIds: messageIds };
+      console.log(data)
+      const url = "/stareds"
+      const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrfToken,
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if(data.starred == true){
+            const alertSuccess = this.successTarget
+            alertSuccess.classList.remove("d-none")
+            fadeOut(alertSuccess)
+          }else{
+            alert("false")
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+      this.checkDivsTargets.forEach(function (element) {
+        element.classList.add('d-none')
+      });
+      this.checklabelTarget.innerText = "Star Messages"
+    }
+  }
+
 }
